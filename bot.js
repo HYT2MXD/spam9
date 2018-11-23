c4580ba5a78c26b096a7f15940c76aa63081ce93
HYT2MXD <39411962+HYT2MXD@users.noreply.github.com>
2018-11-23 18:09:36 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ client.on('ready', async() => {
 var server = "511511291802681344"; // ايدي السررفر
 var channel = "511511291802681348";//ايدي الروم
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('**Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu ,  **')
+    client.guilds.get(server).channels.get(channel).send('**Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu , Desu  **')
     },305);
 })
 
